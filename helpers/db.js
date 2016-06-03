177c13b4432e3c6c1505ad94c469d4cd609cd16a
alekseik <kazachonoka@gmail.com>
2016-06-03 16:31:58 +0300

    change db

--- helpers/db.js
+++ helpers/db.js
@@ -1,6 +1,6 @@
 
 var mongoose = require('mongoose');
 // connect to DB.
-var connection = mongoose.connect('mongodb://localhost/test22');
+var connection = mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds023213.mlab.com:23213/heroku_87cxqr8p');
 
 module.exports = connection;
