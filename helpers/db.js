102988ed6d537051194a20f500f65380eb77a07a
alekseik <kazachonoka@gmail.com>
2016-06-03 16:50:08 +0300

    remove comment

--- helpers/db.js
+++ helpers/db.js
@@ -1,8 +1,7 @@
 
-/*
+
 var mongoose = require('mongoose');
 // connect to DB.
 var connection = mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds015403.mlab.com:15403/heroku_mlcnhf11');
 
 module.exports = connection;
-*/
