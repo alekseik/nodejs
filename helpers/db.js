4cabb20af0ffa367ecafda63fbd37bdcf6086786
alekseik <kazachonoka@gmail.com>
2016-06-03 16:35:12 +0300

    update db string

--- helpers/db.js
+++ helpers/db.js
@@ -1,6 +1,6 @@
 
 var mongoose = require('mongoose');
 // connect to DB.
-var connection = mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds023213.mlab.com:23213/heroku_87cxqr8p');
+var connection = mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds015403.mlab.com:15403/heroku_mlcnhf11');
 
 module.exports = connection;
